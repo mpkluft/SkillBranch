3995bd6cb358adb1b9f598a44af45923c6d26c72
mpkluft <mpkluft@yandex.ru>
2016-11-11 14:02:42 +0300

    add task 2c

--- src/index.js
+++ src/index.js
@@ -16,45 +16,49 @@ app.listen(3000, () => {
 });
 //--------------------------------------------------
 //Урок 2 задача A+B
-import taskAB from './lesson2/AplusB.js';
-
-app.get('/lesson2AplusB', (req, res) => {
-	taskAB(req.query.a, req.query.b);
-	let result = taskAB(req.query.a, req.query.b);
-	res.json({result:result.toString(),
-						task: 'добавить get запрос a=9 b = 7'
-	});
-});
+//import taskAB from './lesson2/AplusB.js';
+//
+//app.get('/lesson2AplusB', (req, res) => {
+//	taskAB(req.query.a, req.query.b);
+//	let result = taskAB(req.query.a, req.query.b);
+//	res.json({result:result.toString(),
+//						task: 'добавить get запрос a=9 b = 7'
+//	});
+//});
 //--------------------------------------------------
 //Урок 2 задача канонизация урлов
-import canonize from './lesson2/canonize.js';
+//import canonize from './lesson2/canonize.js';
 
 app.get('/lesson2canonize', (req, res) => {
-	res.json({
-    url: req.query.url,
-    userName: canonize(req.query.url),
-  });
+	console.log(req.query.username);
+	res.send(
+    canonize(req.query.username),
+  );
 });
+
+function canonize(string) {
+  let userName = string;
+  let reg = new RegExp('@?(https?:)?(\/\/)?(([0-9\w-]*)[^\/]*\/)?@?([A-Za-z0-9._]*)');
+  //if(userName.match(reg)[5].search)
+  return '@' + userName.match(reg)[5];
+}
 //--------------------------------------------------
 //Урок 2 задача спарсить покемонов с сайта pokeapi.co
-import { showPokemons } from './lesson2/pokemon.js';
-
-app.get('/lesson2pokemon', async (req, res) => {
-	showPokemons(res);
-});
+//import { showPokemons } from './lesson2/pokemon.js';
+//
+//app.get('/lesson2pokemon', async (req, res) => {
+//	showPokemons(res);
+//});
 //--------------------------------------------------
 //Урок 2 Перевести полное `Имя Отчество Фамилия` в `Фамилия И. О.`
-//import { showPokemons } from './lesson2/fio.js';
-//?fullname=Владимир Владимирович Путин
-import { chekFio } from './lesson2/fio.js';
-
-app.get('/fio', (req, res) => {
-	let result = chekFio(req.query.fullname);
-	res.send(result._v);
-});
+//import { chekFio } from './lesson2/fio.js';
+//
+//app.get('/fio', (req, res) => {
+//	let result = chekFio(req.query.fullname);
+//	res.send(result._v);
+//});
 
 //--------------------------------------------------
-
 
 ///////////////////////////////
 //точка монтирования корень сайта
--- src/lesson2/canonize.js
+++ src/lesson2/canonize.js
@@ -1,5 +1,11 @@
-export default function canonize(string) {
+app.get('/lesson2canonize', (req, res) => {
+  res.send(
+    canonize(req.query.username),
+  );
+});
+
+function canonize(string) {
   let userName = string;
-  let reg = new RegExp('@?(https?:)?(\/\/)?((telegram|vk|vkontakte|twitter)[^\/]*\/)?([A-Za-z0-9]*)');
-  return userName.match(reg)[5];
+  let reg = new RegExp('@?(https?:)?(\/\/)?(([0-9\w-]*)[^\/]*\/)?@?([A-Za-z0-9._]*)');
+  return '@' + userName.match(reg)[5];
 }
